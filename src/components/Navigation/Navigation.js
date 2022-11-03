487b76c3dd48f7e2c91d879cba0bbad58605d0d3
MaxwellChrist <ribthulu@gmail.com>
2022-11-03 15:49:05 -0400

    built functional navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,48 +1,23 @@
 import { Link, Outlet } from 'react-router-dom'
-import styled from 'styled-components';
-
-const Navbar = styled.div`
-    position: fixed;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    top: 0;
-    width: 100%;
-    height: 50px;
-    background: papayawhip;
-
-    div {
-        display: flex;
-        padding: 0 10px;
-        justify-content: center;
-        gap: 10px;
-
-        a {
-            text-decoration: none;
-            color: green;
-        }   
-
-        h1 {
-            color: blue;
-        }
-    }
-
-`;
+import Logo from '../../assets/images/Logo.png'
+import './Navigation.css'
 
 const Navigation = () => {
     return (
-        <div className='navbar-container' style={{paddingTop: "50px"}}>
-            <Navbar>
-                <div>
+        <div className='navbar-container'>
+            <nav>
+                <div className='navbar-sections'>
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/contact">Contact</Link>
                 </div>
-                <div>
-                    <h1>Logo and social media links for later</h1>
+                <div className='navbar-sections'>
+                    <Link to="/">
+                        <img src={Logo} alt="Logo" />
+                    </Link>
                 </div>
-            </Navbar>
+            </nav>
             <Outlet />
         </div>
     )
