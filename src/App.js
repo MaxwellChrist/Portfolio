a72b77a2e4664e43306f4315bba485d894ce3e54
MaxwellChrist <ribthulu@gmail.com>
2022-11-03 11:46:08 -0400

    setup basic routing for pages that come to mind for portfolio piece; might change pages/components later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+
+// Components
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import NoMatch from './components/NoMatch/NoMatch'
 
 function App() {
   return (
     <div className="App">
-      Start
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
