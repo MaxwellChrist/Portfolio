2394d5aecf75ee2dddc430574055b2ebb0800d96
MaxwellChrist <ribthulu@gmail.com>
2022-11-04 10:27:45 -0400

    added custom loader to page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,43 @@
+import { useState, useEffect } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Logo from '../../assets/images/Logo.png'
 import './Navigation.css'
 
 const Navigation = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }, []);
+
     return (
-        <div className='navbar-container'>
-            <nav>
-                <div className='navbar-sections'>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link>
+        <div className="main-container">
+            {loading ? (
+                <div className="loader-container">
+                    <span className="loader"></span>
                 </div>
-                <div className='navbar-sections'>
-                    <Link to="/">
-                        <img src={Logo} alt="Logo" />
-                    </Link>
+            ) : (
+                <div className='navbar-container'>
+                    <nav>
+                        <div className='navbar-sections'>
+                            <Link to="/">Home</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </div>
+                        <div className='navbar-sections'>
+                            <Link to="/">
+                                <img src={Logo} alt="Logo" />
+                            </Link>
+                        </div>
+                    </nav>
+                    <Outlet />
                 </div>
-            </nav>
-            <Outlet />
+            )}
         </div>
     )
 }
