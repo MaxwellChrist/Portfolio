cd5a445f1606385083d892a6dd4dd2e6c998cf8e
MaxwellChrist <ribthulu@gmail.com>
2022-11-07 09:01:58 -0500

    got rid of some global styling in index.css; messed up my contact page but made my home page work well with desktop

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,15 @@ import Resume from '../../assets/resume/resume.pdf'
 const Home = () => {
     return (
         <>
-            <div className="home-container">
-                <h1>Maxwell Christ</h1>
-                <h3>Full Stack Web Developer</h3>
-                <div class="buttons-container">
-                  <a href={Resume} download className='btn'>Download Resume</a>
+            <section className="home-container">
+                <div className="home-content">
+                    <h1>Maxwell Christ</h1>
+                    <h3>Full Stack Web Developer</h3>
+                    <div className="buttons-container">
+                      <a href={Resume} download className='btn'>Download Resume</a>
+                    </div>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
