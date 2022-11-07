a54a11d080e9e8c4d378697adff5f867eedb872a
MaxwellChrist <ribthulu@gmail.com>
2022-11-07 08:31:53 -0500

    added home page background with button for resume and some styling for desktop

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,10 @@
+import './About.css'
+
 const About = () => {
     return (
         <>
-            <div>
-                <h1>About</h1>
+            <div className="about-container">
+                <h1>This is my about page!</h1>
             </div>
         </>
     )
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import "./Home.css"
+import Resume from '../../assets/resume/resume.pdf'
 
 const Home = () => {
     return (
         <>
             <div className="home-container">
-                <h1>Home</h1>
+                <h1>Maxwell Christ</h1>
+                <h3>Full Stack Web Developer</h3>
+                <div class="buttons-container">
+                  <a href={Resume} download className='btn'>Download Resume</a>
+                </div>
             </div>
         </>
     )
