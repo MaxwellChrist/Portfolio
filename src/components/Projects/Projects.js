cb9067002acc16391f3218e44a7dc35b08f90bac
MaxwellChrist <ribthulu@gmail.com>
2022-11-09 07:05:07 -0500

    added two more projects to projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import Img1 from '../../assets/images/Grid.PNG'
 import Img2 from '../../assets/images/JWT.PNG'
 import Img3 from '../../assets/images/LambdaNews.PNG'
 import Img4 from '../../assets/images/Wheel&Quiz.PNG'
+import Img5 from '../../assets/images/RPS.PNG'
+import Img6 from '../../assets/images/IP.PNG'
 
 const data = [
   {
@@ -29,6 +31,18 @@ const data = [
     title: "React Wheel and Quiz",
     github: 'https://github.com/MaxwellChrist/React-Wheel-and-Quiz'
   },
+  {
+    id: 5,
+    image: Img5,
+    title: "Rock Paper Scissors",
+    github: 'https://github.com/MaxwellChrist/RockPaperScissors'
+  },
+  {
+    id: 6,
+    image: Img6,
+    title: "IP Address Finder",
+    github: 'https://github.com/MaxwellChrist/IP'
+  }
 ]
 
 const Projects = () => {
@@ -39,12 +53,12 @@ const Projects = () => {
         {
           data.map(({id, image, title, github}) => {
             return(
-              <article key={id} className='portfolio-item'>
-                <div className='portfolio-item-image'>
+              <article key={id} className='projects-item'>
+                <div className='projects-item-image'>
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
-                <div className='portfolio-item-links'>
+                <div className='projects-item-links'>
                   <a href={github} className='btn' target="_blank" rel="noreferrer">Github</a>
                 </div>
               </article>
