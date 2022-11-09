e9a65aceaaec77b24659fd66e4b0a1f0fe5a8986
MaxwellChrist <ribthulu@gmail.com>
2022-11-09 08:38:13 -0500

    removed loading screen and edited home component to include gsap animations

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,40 @@
 import "./Home.css"
 import Resume from '../../assets/resume/resume.pdf'
+import {gsap} from 'gsap'
+import {useLayoutEffect, useRef} from 'react'
+
+    // const t1 = gsap.timeline()
+    // gsap.from(['.home-content'], {opacity: 0, duration: 1, y: -50})
 
 const Home = () => {
+
+    const comp = useRef(); // create a ref for the root level element (we'll use it later)
+
+    useLayoutEffect(() => {
+
+        let ctx = gsap.context(() => {
+    
+            // all our animations can use selector text like ".box" 
+            // and it's properly scoped to our component
+            gsap.from(['h1', 'h3'], {opacity: 0, duration: 2, y: -200})
+            
+          }, comp); // <- IMPORTANT! Scopes selector text
+          
+          return () => ctx.revert();
+    
+    }, []);
+
+
     return (
-        <>
-            <section className="home-container">
-                <div className="home-content">
-                    <h1>Maxwell Christ</h1>
-                    <h3>Full Stack Web Developer</h3>
-                    <div className="buttons-container">
-                      <a href={Resume} download className='btn'>Download Resume</a>
-                    </div>
+        <section className="home-container">
+            <div className='home-content' ref={comp}>
+                <h1>Maxwell Christ</h1>
+                <h3>Full Stack Web Developer</h3>
+                <div className="buttons-container">
+                    <a href={Resume} download className='btn'>Download Resume</a>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     )
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 // import { GiHamburgerMenu } from 'react-icons/gi'
 import Logo from '../../assets/images/Logo3.png'
@@ -6,38 +5,23 @@ import './Navigation.css'
 
 const Navigation = () => {
 
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-      }, 2000);
-    }, []);
-
     return (
         <div className="main-container">
-            {loading ? (
-                <div className="loader-container">
-                    <span className="loader"></span>
-                </div>
-            ) : (
-                <header>
-                    <nav>
-                        <div className='navbar-sections'>
-                            <Link id="home" to="/">Home</Link>
-                            <Link id="about" to="/about">About</Link>
-                            <Link id="projects" to="/projects">Projects</Link>
-                            <Link id="contact" to="/contact">Contact</Link>
-                        </div>
-                        <div className='logo navbar-sections'>
-                            <Link to="/">
-                                <img src={Logo} alt="Logo" />
-                            </Link>
-                        </div>
-                    </nav>
-                </header>
-            )}
+            <header>
+                <nav>
+                    <div className='navbar-sections'>
+                        <Link id="home" to="/">Home</Link>
+                        <Link id="about" to="/about">About</Link>
+                        <Link id="projects" to="/projects">Projects</Link>
+                        <Link id="contact" to="/contact">Contact</Link>
+                    </div>
+                    <div className='logo navbar-sections'>
+                        <Link to="/">
+                            <img src={Logo} alt="Logo" />
+                        </Link>
+                    </div>
+                </nav>
+            </header>
             <Outlet />
         </div>
     )
