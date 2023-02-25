86b7afbcceb892f76c2c1e679255749e927aacb0
MaxwellChrist <ribthulu@gmail.com>
2023-02-25 08:59:20 -0500

    added initial mobile format; created hooks to work with screen size and trigger appropriate icon and layout per screen size

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { GrClose } from 'react-icons/gr'
@@ -6,20 +6,26 @@ import Logo from '../../assets/images/Logo3.png'
 import './Navigation.css'
 
 const Navigation = () => {
-
     const [active, setActive] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth)
+
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+      }
+    useEffect(() => {
+        window.addEventListener('resize', handleResize, false);
+        console.log(width)
+    }, [width])
 
     return (
         <div className="app">
             <header>
                 <nav className={active ? "slider active" : "slider"}>
-                    <div className="menu-icon">
-                        <GiHamburgerMenu className='menu' onClick={() => setActive(!active)}/>
+                    <div className="menu-icon" >
+                        <GiHamburgerMenu className='menu' display={!active && width <= 650 ? 'block' : 'none'} onClick={() => setActive(!active)}/>
+                        <GrClose className="close" display={active && width <= 650 ? 'block' : 'none'} onClick={() => setActive(!active)}/>
                     </div>
                     <ul>
-                        <div className="closed">
-                            <GrClose className="close" onClick={() => setActive(!active)}/>
-                        </div>
                         <div className="main-nav">
                             <li><Link id="home" to="/">Home</Link></li>
                             <li><Link id="about" to="/about">About</Link></li>
