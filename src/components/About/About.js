e57978ba182944df1509773f7ecb36c95977a88f
MaxwellChrist <ribthulu@gmail.com>
2023-03-05 10:43:17 -0500

    fixed responsiveness for other screen sizes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,14 +16,15 @@ const About = () => {
             <h1>About Me</h1>
             <p>
               My name is Max Christ and I'm a front end developer with a
-              background in finance. I've always been a dedicated individual who
-              loves learning new technologies and solving problems. Around two
-              years ago, I found my calling to become a developer and I've
-              spent the past year and a half developing my skills through
-              self-teaching, completing a coding bootcamp through BloomTech, and
-              completing a summer apprenticeship with Human Rights First. I will
-              continute to add projects and expand my knowledge while enjoying
-              the process of learning and growing as a front end developer.
+              background in marketing and finance. I've always been a dedicated
+              individual who loves learning new technologies and solving
+              problems. Around two years ago, I found my calling to become a
+              developer and I've spent the past year and a half developing my
+              skills through self-teaching, completing a coding bootcamp through
+              BloomTech, and completing a summer apprenticeship with Human
+              Rights First. I will continute to add projects and expand my
+              knowledge while enjoying the process of learning and growing as a
+              front end developer.
             </p>
           </div>
         </div>
